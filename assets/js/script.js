a51e273f6d6b10e165789b9efe6c4d4840b0f857
10-Andy <meehan.andrew@ymail.com>
2021-08-09 19:02:30 +0000

    Add Searchbar function and event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
-let api = {
-    key: "bd4157381fed90bbfbc805c6db3eaf4b"
-    url: "https://api.openweathermap.org/data/2.5/"
-}
-
+ // Api key and url from weather api 
+const api = {
+    key: "37abfe43d33b5016e5852898f11e9a2e",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  //set up event listener on enter key for the search bar 
+  const searchbar = document.querySelector('.search-bar');
+  searchbar.addEventListener('keypress', searchQuery);
+  
+  function searchQuery(evt) {
+    if (evt.keyCode == 13) {
+      getData(searchbar.value);
+    }
+  }
