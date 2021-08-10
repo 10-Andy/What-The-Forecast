995c0f404e2a06de1203f1e538ab227354f969fa
10-Andy <meehan.andrew@ymail.com>
2021-08-10 19:31:53 +0000

    Add query selector for changing city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,34 @@
  // Api key and url from weather api 
  const api = {
-    key: "37abfe43d33b5016e5852898f11e9a2e",
-    base: "https://api.openweathermap.org/data/2.5/"
-  }
-  //set up event listener on enter key for the search bar 
-  const searchbar = document.querySelector('.search-bar');
-  searchbar.addEventListener('keypress', searchQuery);
-  
-  function searchQuery(evt) {
-    if (evt.keyCode == 13) {
-      getData(searchbar.value);
-    }
-  }
-  // Run a fetch requst on api to return the data in metric units converting it into json
-  function getData (query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then(weather => {
-        return weather.json();
-      }).then(displayData);
-  }
-  
-  function displayData (weather) {
-    console.log(weather);
-}
+   key: "37abfe43d33b5016e5852898f11e9a2e",
+   base: "https://api.openweathermap.org/data/2.5/"
+ }
+
+ //set up event listener on enter key for the search bar 
+ const searchbar = document.querySelector('.search-bar');
+ searchbar.addEventListener('keypress', searchQuery);
+
+ function searchQuery(evt) {
+   if (evt.keyCode == 13) {
+     getData(searchbar.value);
+   }
+ }
+
+ // Run a fetch requst on api to return the data from searchbox in metric units converting it into json
+ function getData(query) {
+   fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+     .then(weather => {
+       return weather.json();
+     }).then(displayData);
+ }
+
+ //Funtion for getting the data for the city searched
+
+ function displayData(weather) {
+
+   //returns the city and country searched by the user
+
+   let city = document.querySelector('#location #city');
+   city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+ 
