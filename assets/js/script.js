7b6704af4d3c4abe3a734585f49ad845ec82a4fc
10-Andy <meehan.andrew@ymail.com>
2021-08-21 18:48:47 +0000

    Clean script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,84 @@
- // Api key and url from weather api 
+ /**Api key and url from weather api*/
  const api = {
-  key: "37abfe43d33b5016e5852898f11e9a2e",
-  base: "https://api.openweathermap.org/data/2.5/"
-};
-const cityRef = document.querySelector('#location #city');
-const dateRef = document.querySelector('#location #day');
-const temperatureRef = document.querySelector('#weather #temperature');
-const forcastRef = document.querySelector('#weather #forcast');
-const rangeRef = document.querySelector('#temp-range');
+   key: "37abfe43d33b5016e5852898f11e9a2e",
+   base: "https://api.openweathermap.org/data/2.5/"
+ };
 
+ /**All the query selectors set up  */
+ const cityRef = document.querySelector('#location #city');
+ const dateRef = document.querySelector('#location #day');
+ const temperatureRef = document.querySelector('#weather #temperature');
+ const forcastRef = document.querySelector('#weather #forcast');
+ const rangeRef = document.querySelector('#temp-range');
 
-//set up event listener on enter key for the search bar 
-const searchbar = document.querySelector('.search-bar');
-searchbar.addEventListener('keypress', searchQuery);
+ /** set up event listener on enter key for the search bar*/
+ const searchbar = document.querySelector('.search-bar');
+ searchbar.addEventListener('keypress', searchQuery);
 
-function searchQuery(evt) {
- if (evt.keyCode === 13 && searchbar.value.length > 0) {
-   getData(searchbar.value);
+ /**Checking to see if there has been a location typed into the search bar before enter was hit  */
+ function searchQuery(evt) {
+   if (evt.keyCode === 13 && searchbar.value.length > 0) {
+     getData(searchbar.value);
+   }
  }
-}
 
-function getData(query) {
- alert("Called the getData() function.");
-}
-// Run a fetch requst on api to return the data from searchbox in metric units converting it into json
+ function getData(query) {
+   alert("Called the getData() function.");
+ }
 
-function getData(query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => weather.json()).then(displayData).catch(error => {
-      alert("Ooops Maybe Check Your Spelling?");
-    });
-}
-//Funtion for getting the data for the city searched
+ /**Run a fetch requst on api to return the data from searchbox in metric units converting it into json
+  * Catching the error and alerting the user when they have misspelled the city name
+  */
+ function getData(query) {
+   fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+     .then(weather => weather.json()).then(displayData).catch(error => {
+       alert("Ooops Maybe Check Your Spelling?");
+     });
+ }
 
-function displayData(weather) {
+ /**Funtion for getting the data for the city searched and changhing the inner HTML */
+ function displayData(weather) {
 
-  cityRef.innerText = `${weather.name}, ${weather.sys.country}`;
+   cityRef.innerText = `${weather.name}, ${weather.sys.country}`;
 
-  let today = new Date();
+   let today = new Date();
 
-  dateRef.innerText = todaysDate(today);
+   dateRef.innerText = todaysDate(today);
 
-  temperatureRef.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+   temperatureRef.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
-  forcastRef.innerText = weather.weather[0].main;
+   forcastRef.innerText = weather.weather[0].main;
 
-  rangeRef.innerText = `${Math.round(weather.main.temp_min)}°c Min / ${Math.round(weather.main.temp_max)}°c Max`;
+   rangeRef.innerText = `${Math.round(weather.main.temp_min)}°c Min / ${Math.round(weather.main.temp_max)}°c Max`;
 
+   /**Gets the Id from the weather API and selects the correct weather icon based on the id returned*/
+   let weatherIconId = `${weather.weather[0].id}`;
+   let weatherIcon = document.getElementById("temp-icon-img");
 
-  let weatherIconId = `${weather.weather[0].id}`;
-  let weatherIcon = document.getElementById("temp-icon-img");
+   if (weatherIconId < 250) {
+     weatherIcon.src = 'assets/images/weather-images/thunder.png';
+   } else if (weatherIconId < 550) {
+     weatherIcon.src = 'assets/images/weather-images/rain.png';
+   } else if (weatherIconId < 650) {
+     weatherIcon.src = 'assets/images/weather-images/snow.png';
+   } else if (weatherIconId < 800) {
+     weatherIcon.src = 'assets/images/weather-images/clear.png';
+   } else if (weatherIconId === 800) {
+     weatherIcon.src = 'assets/images/weather-images/sun.png';
+   } else if (weatherIconId > 800) {
+     weatherIcon.src = 'assets/images/weather-images/cloudy.png';
+   }
+ }
 
-  if (weatherIconId < 250) {
-    weatherIcon.src = 'assets/images/weather-images/thunder.png';
-  } else if (weatherIconId < 550) {
-    weatherIcon.src = 'assets/images/weather-images/rain.png';
-  } else if (weatherIconId < 650) {
-    weatherIcon.src = 'assets/images/weather-images/snow.png';
-  } else if (weatherIconId < 800) {
-    weatherIcon.src = 'assets/images/weather-images/clear.png';
-  } else if (weatherIconId === 800) {
-    weatherIcon.src = 'assets/images/weather-images/sun.png';
-  } else if (weatherIconId > 800) {
-    weatherIcon.src = 'assets/images/weather-images/cloudy.png';
-  }
-}
+ /**Function to select the full day, date, month and year */
+ function todaysDate(todaysFullDate) {
+   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-//Function to select the full day, date, month and year
+   let day = days[todaysFullDate.getDay()];
+   let date = todaysFullDate.getDate();
+   let month = months[todaysFullDate.getMonth()];
+   let year = todaysFullDate.getFullYear();
 
-function todaysDate(todaysFullDate) {
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  let day = days[todaysFullDate.getDay()];
-  let date = todaysFullDate.getDate();
-  let month = months[todaysFullDate.getMonth()];
-  let year = todaysFullDate.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`;
-}
+   return `${day} ${date} ${month} ${year}`;
+ }
